a3fda8ea50ecda6cf522b8dda1e48bd24e44dd1a
moshesilver <moshetzvi03@gmail.com>
2023-07-10 19:37:45 -0400

    most of program

--- Runner_Excercise/Program.cs
+++ Runner_Excercise/Program.cs
@@ -1,1 +1,104 @@
-﻿
+﻿namespace Runner_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating Runner objects in array
+            Runner[] runners = new Runner[]
+            {
+                new Runner("Steve", 12, 33),
+                new Runner("Tim", 34, 28),
+                new Runner("Mark", 22, 37),
+                new Runner("Tom", 21, 30),
+                new Runner("Cliff", 13, 33),
+                new Runner("Vini", 17, 28),
+                new Runner("Matt", 10, 28),
+                new Runner("Ben", 9, 29),
+                new Runner("Brandon", 15, 14)
+            };
+
+            //Swap function declaration
+            static void Swap<T>(T[] array, int index1, int index2)
+            {
+                T temporary = array[index1];
+                array[index1] = array[index2];
+                array[index2] = temporary;
+            }
+
+            //reorder based on time
+            int q = 0;
+            while (q < runners.Length)
+            {
+                int i = 0;
+                foreach (Runner r in runners)
+                {
+                    if (i == 0) { }
+                    else if (r.Time < runners[i - 1].Time)
+                    {
+                        Swap(runners, i, i - 1);
+                    }
+
+                    i++;
+                }
+
+                q++;
+            }
+
+            //add age group based rankings
+            foreach (Runner r in runners)
+            {
+                if (r.Age <= 15)
+                {
+
+                }
+                else if (r.Age > 15 &&  r.Age < 30)
+                {
+                     
+                }
+                else if (r.Age >= 30)
+                {
+
+                }
+            }
+
+            //display results
+            int n = 1;
+            foreach (Runner r in runners)
+            {
+                Console.WriteLine($"Runner #{n}     {r.Name}      {r.Time}      {r.Age}     {r.Ranking}");
+                n++;
+            }
+
+
+
+
+            /* 
+            Runner Steve = new Runner("Steve", 12, 33);
+            Runner Tim = new Runner("Tim", 34, 28);
+            Runner Mark = new Runner("Mark", 22, 37);
+            Runner Tom = new Runner("Tom", 21, 30);
+            Runner Cliff = new Runner("Cliff", 13, 33);
+            Runner Vini = new Runner("Vini", 17, 28);
+            Runner Matt = new Runner("Matt", 10, 28);
+            Runner Ben = new Runner("Ben", 9, 29);
+            Runner Brandon = new Runner("Brandon", 15, 14);
+            */
+
+            /* 
+            List<Runner> runners = new List<Runner>();
+            runners.Add(new Runner { Name = "Steve", Time = 12, Age = 33 });
+            runners.Add(new Runner { Name = "Tim", Time = 34, Age = 28 });
+            runners.Add(new Runner { Name = "Mark", Time = 22, Age = 37 });
+            runners.Add(new Runner { Name = "Tom", Time = 21, Age = 30 });
+            runners.Add(new Runner { Name = "Cliff", Time = 13, Age = 33 });
+            runners.Add(new Runner { Name = "Vini", Time = 17, Age = 28 });
+            runners.Add(new Runner { Name = "Matt", Time = 10, Age = 28 });
+            runners.Add(new Runner { Name = "Ben", Time = 9, Age = 29 });
+            runners.Add(new Runner { Name = "Brandon", Time = 15, Age = 14 });
+            */
+
+
+        }
+    }
+}
---
+++ Runner_Excercise/Runner.cs
@@ -0,0 +1,58 @@
+﻿using System.Dynamic;
+
+namespace Runner_Exercise
+{
+    class Runner
+    {
+
+        //FIELDS
+        private static int runnerCount;
+        private string name;
+        private int time;
+        private int age;
+        private int ranking;
+
+        //CONSTRUCTORS
+        public Runner(string name, int time, int age)
+        {
+            this.Name = name;
+            this.Time = time;
+            this.Age = age;
+            runnerCount++;
+        }
+
+        static Runner()
+        {
+            runnerCount = 0;
+        }
+
+        //PROPERTIES
+        public static int RunnerCount
+        {
+            get { return runnerCount; }
+            set { runnerCount = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Time
+        {
+            get { return time; }
+            set { time = value; }   
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public int Ranking
+        {
+            get { return ranking; }
+            set { ranking = value; } 
+        }
+
+    }
+
+}
